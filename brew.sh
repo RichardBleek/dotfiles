1d9ca0d9e02513f4401c54acd05f5afab0162be0
Richard <richard.bleek@ing.com>
2019-05-07 14:03:13 +0200

    update brew file

--- brew.sh
+++ brew.sh
@@ -5,6 +5,7 @@ brew install python
 brew install tree
 brew install ripgrep
 brew install git
+brew install git-flow
 brew install ansible
 brew install jenv
 
@@ -18,8 +19,6 @@ brew cask install virtualbox
 brew cask install sourcetree
 brew cask install discord
 brew cask install vlc
-brew cask install hyperdock
-brew cask install divvy
 
 # Install Source Code Pro Font
 brew tap homebrew/cask-fonts
